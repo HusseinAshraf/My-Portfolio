17d608e136ff80dd1f8be361700be5e70a5cfbc9
HusseinAshraf <husseinashraf7414@gmail.com>
2024-12-23 01:11:11 +0200

    update Footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,10 @@
 function Footer() {
   const date = new Date();
-  const year = date.getFullYear()
+  const year = date.getFullYear();
 
   return (
-    <footer className="relative z-20 w-full bg-[#7508E8] py-5">
-      <div className=" text-white flex w-full items-center justify-center font-semibold sm:text-sm md:text-xl">
+    <footer className="relative z-20 w-full bg-gradient-to-r from-[#6A1DFF] via-[#4C00B5] to-[#2C0077] py-6">
+      <div className="text-white flex w-full items-center justify-center font-semibold text-sm sm:text-base md:text-lg">
         &copy; {year} Hussein. A, All rights reserved.
       </div>
     </footer>
