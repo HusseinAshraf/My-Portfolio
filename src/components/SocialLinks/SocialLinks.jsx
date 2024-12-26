382a08747832df37b707759a8cdc86542c4e0b8d
HusseinAshraf <husseinashraf7414@gmail.com>
2024-12-26 18:12:39 +0200

    update button

--- src/components/SocialLinks/SocialLinks.jsx
+++ src/components/SocialLinks/SocialLinks.jsx
@@ -30,7 +30,7 @@ const SocialLinks = () => {
       id: 4,
       label: "Resume",
       icon: <BsFillPersonLinesFill size={30} />,
-      href: "/src/assets/resume/my-resume.pdf", 
+      href: "/public/resume/my-resume.pdf", 
       style: "rounded-br-md",
       download: true,
     },
